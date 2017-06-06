9ca9d2fab1008990d97df63a16dd38cbefa5ba96
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-06 08:59:08 -0500

    screwing up my js added i < arrayLength and I dont understand why it wont work

--- overly-exctied.js
+++ overly-exctied.js
@@ -1,5 +1,5 @@
 // Create an array that contains the words in the sentence
-var sentence = ["Adding Excitement to JavaScript"];
+var sentence = ["Adding", "Excitement", "to", "JavaScript"];
 
 // The addExcitement function should accept the array as the sole argument
 function addExcitement (theWordArray) {
@@ -7,17 +7,15 @@ function addExcitement (theWordArray) {
       Write a `for` loop that iterates over the array argument and
       outputs the words.
      */
-     for(var i = 0; i >= 3; i++){
-     	console.log("sentence", addExcitement);
+     for(var i = 0; i = arrayLength; i++){
      };
 }
+     	console.log();
 
 // Invoke the function and pass in the array
 addExcitement(sentence)
 
-	for(var i = 0; i>=3; i = i 
-		+3) {
-		console.log("! every 3", sentence );
-	};
+	//for(var i = 0; i; i = i 
+		//+3) {
+		//console.log("! every 3", sentence );};
 
-	
