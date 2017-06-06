4f3b734dc3a0b9201addb6b9418c1965b5f59395
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-06 14:22:50 -0500

    getting the sentace (as an array) to print to the console as a string with join

---
+++ overly-excited.js
@@ -0,0 +1,23 @@
+// Create an array that contains the words in the sentence
+let sentence = ["I", "am", "adding", "a", "sentence", "with", "excitement"];
+
+let joinedSentance = sentence.join(" ")
+console.log("sentance", joinedSentance);
+// The addExcitement function should accept the array as the sole argument
+function addExcitement (joinedSentance) {
+    /*
+      Write a `for` loop that iterates over the array argument and
+      outputs the words.
+     */
+     for(var i = 0; i > sentence.length; i++){
+        console.log("sentace", sentace[i] );
+     };
+}
+     	
+
+// Invoke the function and pass in the array
+//addExcitement(sentence)
+
+	//for(var i = 0; i; i = i 
+		//+3) {
+		//console.log("! every 3", sentence );};
