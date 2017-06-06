f8b17b1ed8c46caaf2d3c09b9b86d0abee836e35
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-06 15:36:06 -0500

    getting closer to finishing this exercise- done 2 of 3- tring to add ! every 3 with for loop...

--- overly-excited.js
+++ overly-excited.js
@@ -1,23 +1,29 @@
 // Create an array that contains the words in the sentence
-let sentence = ["I", "am", "adding", "a", "sentence", "with", "excitement"];
+let sentance = ["I", "am", "adding", "a", "sentence", "with", "excitement"];
 
-let joinedSentance = sentence.join(" ")
-console.log("sentance", joinedSentance);
 // The addExcitement function should accept the array as the sole argument
-function addExcitement (joinedSentance) {
+function addExcitement (sentance) {
     /*
       Write a `for` loop that iterates over the array argument and
       outputs the words.
      */
-     for(var i = 0; i > sentence.length; i++){
-        console.log("sentace", sentace[i] );
+     for(var i = 0; i > sentance.length; i++){
+        return `"${sentance.length[i]}`
      };
 }
-     	
+      console.log("sentance?", sentance );
+
+//joining the sentace
+let joinedSentance = sentance.join(" ")
+console.log("sentance", joinedSentance);
 
 // Invoke the function and pass in the array
 //addExcitement(sentence)
 
-	//for(var i = 0; i; i = i 
-		//+3) {
-		//console.log("! every 3", sentence );};
+	for(var i = 0; i > joinedSentance.length; i = i +3) {
+    if (i == joinedSentance[i]) {
+      console.log("blah", `"${joinedSentance[i]}" !`);
+    }
+};
+
+//console.log("! every 3?", `${joinedSentance}` );
