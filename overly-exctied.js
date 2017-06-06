30177003bab05837d40fc9a5d944b4b7a24b7b4b
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-06 08:36:56 -0500

    adding java scriptbut the js file is not linking to html... trying to figure out why

---
+++ overly-exctied.js
@@ -0,0 +1,23 @@
+// Create an array that contains the words in the sentence
+var sentence = ["Adding Excitement to JavaScript"];
+
+// The addExcitement function should accept the array as the sole argument
+function addExcitement (theWordArray) {
+    /*
+      Write a `for` loop that iterates over the array argument and
+      outputs the words.
+     */
+     for(var i = 0; i >= 3; i++){
+     	console.log("sentence", addExcitement);
+     };
+}
+
+// Invoke the function and pass in the array
+addExcitement(sentence)
+
+	for(var i = 0; i>=3; i = i 
+		+3) {
+		console.log("! every 3", sentence );
+	};
+
+console.log("hellow", hellow);
